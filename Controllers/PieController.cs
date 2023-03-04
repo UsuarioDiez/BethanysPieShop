413a627010ffa522855f58e4ff3d239a998366b1
Marco Zuñiga <alex13_hbk@hotmail.com>
2023-03-04 16:28:02 -0600

    Add Details view and add Models to ViewImport

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -15,9 +15,16 @@ namespace BethanysPieShop.Controllers
         }
 
         public IActionResult List()
-        {
+        { 
             PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
             return View(piesListViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null) return NotFound();
+            return View(pie);
         }
     }
 }
